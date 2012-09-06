0fb249d3712e7f59f9e5052e8e6d6122aca01d1f
AAA <asdas@asda.de>
2012-09-06 15:12:29 +0200

    adding more decorators

---
+++ WordProcessor.py
@@ -0,0 +1,35 @@
+
+
+class WordProcessor(object):
+    PLUGINS = []
+    
+    @classmethod
+    def process(cls, text):
+        for plugin in cls.PLUGINS:
+            text= plugin().cleanup(text)
+        return text
+
+    @classmethod
+    def plugin(cls, plgn):
+        cls.PLUGINS.append(plgn)
+
+
+
+@WordProcessor.plugin
+class CleanMDashesExtension(object):
+        def cleanup(self, text):
+            return text.replace('&mdash;', u'\N{em dash}')
+
+
+@WordProcessor.plugin
+class CleanUpperCase(object):
+        def cleanup(self,text):
+            return text[-1]
+
+
+print WordProcessor.process("Hi this is a Test")
+
+
+
+
+
